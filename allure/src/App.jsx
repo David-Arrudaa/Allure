a757bf233f119098fbce17b94100adcf1a1a37c1
David Arruda <168621143+David-Arrudaa@users.noreply.github.com>
2026-08-03 20:13:08 +0000

    chore: setup inicial da arquitetura de pastas e css global

---
+++ allure/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Allure Gestão</h1>
+      <p>Sistema inteligente para salões de beleza.</p>
+    </div>
+  );
+}
+
+export default App;
